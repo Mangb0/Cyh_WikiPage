d04ff81d015d8e7c06e36429dc605f0acfd65a62
Mangbo <whdudghks45629@gmail.com>
2024-02-17 01:46:24 +0900

    page 기본 세팅

---
+++ src/components/WikipageDetail.jsx
@@ -0,0 +1,9 @@
+const WikipageDetail = () => {
+  return (
+    <div>
+      <h1>Wikipage Detail</h1>
+    </div>
+  );
+};
+
+export default WikipageDetail;
---
+++ src/components/WikipageList.jsx
@@ -0,0 +1,9 @@
+const WikipageList = () => {
+  return (
+    <div>
+      <h1>WikipageList</h1>
+    </div>
+  );
+};
+
+export default WikipageList;
